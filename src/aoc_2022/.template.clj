0dd221a43597b3ad4617e03f959e57896c1cbc62
Einārs Lielmanis <einars@spicausis.lv>
2022-12-03 12:04:44 +0200

    template upd

--- src/aoc_2022/.template.clj
+++ src/aoc_2022/.template.clj
@@ -1,10 +1,26 @@
-(ns aoc-2022.day2
+(ns aoc-2022.dayN
   (:require [clojure.test :as test :refer [deftest is]])
   (:require [aoc-2022.helpers :as h]))
 
-(defn solve-1 []
-  )
+(defn solve-1
+  ([] (solve-1 "resources/dayN.txt"))
+  ([file]
+    (->>
+      (h/slurp-strings file)
+      ; ...
+      )))
 
-(defn solve-2 []
-  )
+(defn solve-2
+  ([] (solve-2 "resources/dayN.txt"))
+  ([file]
+    (->>
+      (h/slurp-strings file)
+      ; ...
+      )))
 
+(deftest test [] 
+  (test/are [x y] (= x y)
+    true false
+    ; 0 (solve-1 "resources/dayN.test.txt")
+    ; 0 (solve-2 "resources/dayN.test.txt")
+    ))
