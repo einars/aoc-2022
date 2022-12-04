1074a28334c3c158a69b81792322cff91b6aa490
Einārs Lielmanis <einars@spicausis.lv>
2022-12-04 12:56:39 +0200

    day 4 pt 2

--- src/aoc_2022/day4.clj
+++ src/aoc_2022/day4.clj
@@ -22,6 +22,13 @@
   (or (<= f1 f2 t2 t1)
       (<= f2 f1 t1 t2)))
 
+(defn ranges-overlap?
+  [[f1 t1] [f2 t2]]
+  (or (<= f1 f2 t1)
+      (<= f1 t2 t1)
+      (<= f2 f1 t2)
+      (<= f2 t1 t2)))
+
 (defn solve-1
   ([] (solve-1 "resources/day4.txt"))
   ([file]
@@ -31,13 +38,14 @@
       (filter #(ranges-fully-contain-any? (first %) (second %)))
       count)))
 
-(comment defn solve-2
+(defn solve-2
   ([] (solve-2 "resources/day4.txt"))
   ([file]
     (->>
       (h/slurp-strings file)
-      ; ...
-      )))
+      (map convert-to-ranges)
+      (filter #(ranges-overlap? (first %) (second %)))
+      count)))
 
 
 (deftest test [] 
@@ -46,7 +54,7 @@
     true (ranges-fully-contain-any? '(6 6) '(4 6))
     true (ranges-fully-contain-any? '(4 6) '(6 6))
     2 (solve-1 "resources/day4.test.txt")
-    ; 0 (solve-2 "resources/day4.test.txt")
+    4 (solve-2 "resources/day4.test.txt")
     ))
 
 (test/run-tests)
