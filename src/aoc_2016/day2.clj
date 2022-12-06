57cee885f091e253de47d941502bbb5aab430c3b
Einārs Lielmanis <einars@spicausis.lv>
2022-12-07 00:11:25 +0200

    2016 Day 2 pt 2

--- src/aoc_2016/day2.clj
+++ src/aoc_2016/day2.clj
@@ -1,28 +1,45 @@
 (ns aoc-2016.day2
   (:require [clojure.test :as test :refer [deftest]])
   (:require [clojure.string :as str])
-  (:require [clojure.tools.trace :as trace])
+  (:require [clojure.tools.trace :as trace :refer[deftrace trace]])
+  (:require [clojure.pprint :as pprint])
   (:require [aoc.helpers :as h]))
 
-(def moves
-  {1 {      \R 2   \D 4       }
-   2 {      \R 3   \D 5   \L 1}
-   3 {             \D 6   \L 2}
-   4 {\U 1  \R 5   \D 7       }
-   5 {\U 2  \R 6   \D 8   \L 4}
-   6 {\U 3         \D 9   \L 5}
-   7 {\U 4  \R 8              }
-   8 {\U 5  \R 9          \L 7}
-   9 {\U 6                \L 8}
-  })
+(defn tpl-get-in
+  "same as get-in, but returns nil on \\space"
+  [m ks]
+  (let [ret (get-in m ks)]
+    (when (not= ret \space) ret)))
+
+(defn template->moves
+  [template]
+  (into {}
+    (for [x (range 5)
+          y (range 5)
+          :let [ch (get-in template [y x])]
+          :when (not= \space ch)]
+
+          [ ch {\U (tpl-get-in template [(dec y) x])
+                \D (tpl-get-in template [(inc y) x])
+                \L (tpl-get-in template [y (dec x)])
+                \R (tpl-get-in template [y (inc x)])}]
+          )))
+
+(def moves-1 (template->moves ["     " " 123 " " 456 " " 789 " "     "]))
+(def moves-2 (template->moves ["  1  " " 234 " "56789" " ABC " "  D  "]))
+
+(def ^:dynamic *moveset* moves-1)
 
 (defn move 
-  "ex: (move 5 \\U)"
+  "ex: (move \5 \\U)"
   [what dir]
- (get (moves what) dir what))
+  (if-some [res (get (*moveset* what) dir)]
+    res
+    what))
+
 
 (defn walk-code
-  "ex: (walk-code 5 [\\U \\L \\L])"
+  "ex: (walk-code \5 [\\U \\L \\L])"
   [starting-digit code]
   (reduce move starting-digit code))
 
@@ -37,25 +54,27 @@
 (defn solve-1
   ([] (solve-1 "resources/2016/day2.txt"))
   ([file]
-    (->>
-      (h/slurp-strings file)
-      (walk-codes 5)
-      str/join)))
+    (binding [*moveset* moves-1]
+      (->>
+        (h/slurp-strings file)
+        (walk-codes \5)
+        str/join))))
 
-(comment defn solve-2
+(defn solve-2
   ([] (solve-2 "resources/2016/day2.txt"))
   ([file]
-    (->>
-      (h/slurp-strings file)
-      ; ...
-      )))
+    (binding [*moveset* moves-2]
+      (->>
+        (h/slurp-strings file)
+        (walk-codes \5)
+        str/join))))
 
 (deftest test-stuff [] 
   (test/are [x y] (= x y)
-    2 (move 5 \U)
-    1 (move 1 \L)
-    1 (walk-code 5 "ULL")
-    [1 9] (walk-codes 5 ["ULL", "RRDDD"])
+    \2 (move \5 \U)
+    \1 (move \1 \L)
+    \1 (walk-code \5 "ULL")
+    [\1 \9] (walk-codes \5 ["ULL", "RRDDD"])
     "1985" (solve-1 "resources/2016/day2.test.txt")
-    ; 0 (solve-2 "resources/2016/day2.test.txt")
+    "5DB3" (solve-2 "resources/2016/day2.test.txt")
     ))
