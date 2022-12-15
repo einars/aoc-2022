10b69eb9e2c736a5fd6a23f5e162a9d40e300f01
Einārs Lielmanis <einars@spicausis.lv>
2022-12-15 14:52:02 +0200

    2022 day 15 pt 2

--- src/aoc_2022/day15.clj
+++ src/aoc_2022/day15.clj
@@ -8,46 +8,74 @@
     [aoc.helpers :as h]))
 
 (defn parse [l]
-  (->> l
-    (re-seq #"-?\d+")
-    (mapv #(Integer/parseInt %))))
+  (let [[sx sy bx by] (->> l
+                        (re-seq #"-?\d+")
+                        (mapv #(Integer/parseInt %)))]
+    [sx sy (+ (abs (- bx sx)) (abs (- by sy))) bx by] ))
 
 (defn get-busy-range [beacon target-y]
-  (let [[sx sy bx by] beacon
-        safe-distance (+ (abs (- bx sx)) (abs (- by sy)))
+  (let [[sx sy dist] beacon
         dist-at-y (abs (- target-y sy))]
 
-    (when (<= dist-at-y safe-distance)
-      (let [side (- safe-distance dist-at-y)]
-        [(- sx side) (+ sx side) :bea beacon :safe safe-distance :dist dist-at-y]))))
+    (when (<= dist-at-y dist)
+      (let [side (- dist dist-at-y)]
+        [(- sx side) (+ sx side)]))))
 
 (defn solve-for
   [y beacons]
   (let [intervals (keep #(get-busy-range % y) beacons)
         beacon-xs-at-y (->> beacons
-                         (filter #(= y (nth % 3)))
-                         (map #(nth % 2)))
-        sensor-xs-at-y (->> beacons
-                         (filter #(= y (nth % 1)))
-                         (map #(nth % 0)))
-        ]
-    (count (set/difference 
-      (into #{} (sort (mapcat #(range (first %) (inc (second %))) intervals)))
-      (into #{} beacon-xs-at-y)
-      (into #{} sensor-xs-at-y)))))
+                         (filter #(= y (nth % 4)))
+                         (map #(nth % 3)))]
+    (set/difference 
+      (into #{} (mapcat #(range (first %) (inc (second %))) intervals))
+      (into #{} beacon-xs-at-y))))
+
+(defn in-safe-range-one? [[x y] [sx sy dist bx by]]
+  (let [our-distance (+ (abs (- x sx)) (abs (- y sy)))]
+    (or (<= our-distance dist) (and (= x bx) (= y by)))))
+
+(defn in-safe-range? [xy beacons]
+  (some #(in-safe-range-one? xy %) beacons))
+
+(defn get-outer-border [[sx sy dist]] 
+  "all the coordinates immediately outside the safe range"
+  (let [dist (inc dist)]
+    (apply concat (for [n (range 0 (inc dist))]
+                    [[(+ sx (- dist n)) (- sy n)]
+                     [(+ sx (- dist n)) (+ sy n)]
+                     [(- sx (- dist n)) (- sy n)]
+                     [(- sx (- dist n)) (+ sy n)]]))))
+
+(defn find-distress [in-range? beacons]
+  (->> beacons
+    (mapcat get-outer-border)
+    (filter (fn [[x y]] (and (in-range? x) (in-range? y)))) ; clip to requested coordinates
+    (filter #(not (in-safe-range? % beacons))) ; remove coordinates deemed safe
+    first))
+
+(defn calc-tuning [[x y]] (+ y (* 4000000 x)))
 
 (defn solve-1
   ([] (solve-1 "resources/2022/day15.txt" 2000000))
   ([file y]
+   (->> file
+     (h/slurp-strings)
+     (map parse)
+     (solve-for y)
+     count)))
+
+(defn solve-2
+  ([] (solve-2 "resources/2022/day15.txt" #(< 0 % 4000000)))
+  ([file in-range?]
    (->>
      (h/slurp-strings file)
      (map parse)
-     (solve-for y))))
-
+     (find-distress in-range?)
+     (calc-tuning))))
 
 (deftest test-stuff [] 
   (test/are [x y] (= x y)
-    27 (solve-1 "resources/2022/day15.test.txt" 11)
     26 (solve-1 "resources/2022/day15.test.txt" 10)
     25 (solve-1 "resources/2022/day15.test.txt" 9)
-    ))
+    56000011 (solve-2 "resources/2022/day15.test.txt" #(< 0 % 20))))
