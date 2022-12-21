bdbd3ac304fe51cc6a27a9ab0b504abd77906ee5
Einārs Lielmanis <einars@spicausis.lv>
2022-12-21 07:58:07 +0200

    2022 day 21 pt 2

--- src/aoc_2022/day21.clj
+++ src/aoc_2022/day21.clj
@@ -25,10 +25,22 @@
     (cond
       (number? x) x
       :else 
-      (eval (list (first x) (monkey-eval (second x) all) (monkey-eval (nth x 2) all))) )) )
-
+      (eval (list (first x)
+              (monkey-eval (second x) all) 
+              (monkey-eval (nth x 2) all))))))
 
 (def monkey-eval (memoize monkey-eval-impl))
+
+(defn simplify [what all]
+  (let [x (all what)]
+    (cond
+      (= what :humn) :humn
+      (number? x) x
+      :else (let [a (simplify (second x) all)
+                  b (simplify (nth x 2) all)]
+              (if (and (number? a) (number? b))
+                (eval (list (first x) a b))
+                [(first x) a b])))))
 
 (defn solve-1
   ([] (solve-1 "resources/2022/day21.txt"))
@@ -39,10 +51,42 @@
      (into {})
      (monkey-eval :root))))
 
-;(solve-1 "resources/2022/day21.test.txt")
-(solve-1)
+(defn rev-eval [expr n]
+  (if (= expr :humn)
+    n
+    (let [[e n1 n2] expr]
+      (cond
+        (and (= '+ e) (number? n1)) (rev-eval n2 (- n n1))
+        (and (= '+ e) (number? n2)) (rev-eval n1 (- n n2))
+
+        (and (= '- e) (number? n1)) (rev-eval n2 (- n1 n))
+        (and (= '- e) (number? n2)) (rev-eval n1 (+ n2 n))
+
+        (and (= '* e) (number? n1)) (rev-eval n2 (/ n n1))
+        (and (= '* e) (number? n2)) (rev-eval n1 (/ n n2))
+
+        (and (= '/ e) (number? n1)) (rev-eval n2 (/ n1 n))
+        (and (= '/ e) (number? n2)) (rev-eval n1 (* n n2))))))
+
+(defn do-part-2 [all]
+  (let [[_ root-lt root-rt] (all :root)
+        lt (simplify root-lt all)
+        rt (simplify root-rt all)]
+
+    (if (number? lt) 
+      (rev-eval rt lt)
+      (rev-eval lt rt))))
+
+(defn solve-2
+  ([] (solve-2 "resources/2022/day21.txt"))
+  ([file]
+   ( ->>
+     (h/slurp-strings file)
+     (map parse)
+     (into {})
+     (do-part-2))))
 
 (deftest test-stuff [] 
   (test/are [x y] (= x y)
     152 (solve-1 "resources/2022/day21.test.txt")
-    ))
+    301 (solve-2 "resources/2022/day21.test.txt")))
