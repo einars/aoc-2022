e658cc27b9ee7b0eedc87ebab13e9f1d0c5d0867
Einārs Lielmanis <einars@spicausis.lv>
2023-01-01 04:00:03 +0200

    2020/9.2

--- src/aoc_2020/day9.clj
+++ src/aoc_2020/day9.clj
@@ -26,6 +26,30 @@
      first
      last)))
 
+(defn find-partition-summing-to 
+  ([xs n] (find-partition-summing-to xs n 0))
+  ([xs n idx-start]
+   (loop [idx idx-start sum 0]
+     (cond
+       (= sum n) (->> xs (drop idx-start) (take (- idx idx-start)))
+       (= idx (count xs)) (find-partition-summing-to xs n (inc idx-start))
+       (> sum n) (find-partition-summing-to xs n (inc idx-start))
+       :else (recur (inc idx) (+ sum (get xs idx)))))))
+
+(defn solve-2
+  ([] (solve-2 "resources/2020/day9.txt"))
+  ([file]
+   (let [numbers (vec (h/slurp-ints file))
+         crap-number (->> numbers
+                       (partition (inc *preamble-count*) 1 (repeat 0))
+                       (drop-while matches-preamble?)
+                       first
+                       last)
+         partition (find-partition-summing-to numbers crap-number)
+         ]
+     (+ (apply min partition) (apply max partition)))))
+
 (deftest test-stuff [] 
   (test/are [x y] (= x y)
-    127 (binding [*preamble-count* 5] (solve-1 "resources/2020/day9.test.txt"))))
+    127 (binding [*preamble-count* 5] (solve-1 "resources/2020/day9.test.txt"))
+    62 (binding [*preamble-count* 5] (solve-2 "resources/2020/day9.test.txt"))))
