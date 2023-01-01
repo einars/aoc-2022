12e06c29b85d0c3d1ae46d9e3b73960d3b2e4375
Einārs Lielmanis <einars@spicausis.lv>
2023-01-01 04:30:01 +0200

    2020/10.2

--- src/aoc_2020/day10.clj
+++ src/aoc_2020/day10.clj
@@ -12,6 +12,18 @@
         grouped (group-by identity diffs)]
     (* (count (get grouped 3 0)) (count (get grouped 1 0)))))
 
+
+(def arrangements)
+
+(defn arrangements-impl [n xs]
+  (let [res (+ 
+              (if (xs (+ n 1)) (arrangements (+ n 1) xs) 0)
+              (if (xs (+ n 2)) (arrangements (+ n 2) xs) 0)
+              (if (xs (+ n 3)) (arrangements (+ n 3) xs) 0))]
+    (if (= 0 res) 1 res)))
+
+(def arrangements (memoize arrangements-impl))
+
 (defn add-0 [xs] (conj xs 0))
 
 (defn solve-1
@@ -23,10 +35,19 @@
      (sort >)
      (partition 2 1)
      (map (partial apply -))
-     get-answer
-     )))
+     get-answer)))
+
+(defn solve-2
+  ([] (solve-2 "resources/2020/day10.txt"))
+  ([file]
+   (->>
+     (h/slurp-ints file)
+     add-0
+     set
+     (arrangements 0))))
 
 (deftest test-stuff [] 
   (test/are [x y] (= x y)
     35 (solve-1 "resources/2020/day10.test.txt")
-    220 (solve-1 "resources/2020/day10.test2.txt")))
+    220 (solve-1 "resources/2020/day10.test2.txt")
+    19208 (solve-2 "resources/2020/day10.test2.txt")))
