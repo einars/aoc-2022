aae14d9a5ff17eb396e09f0adf586bcefea427ae
Einārs Lielmanis <einars@spicausis.lv>
2023-01-01 07:09:57 +0200

    2020/12.2

--- src/aoc_2020/day12.clj
+++ src/aoc_2020/day12.clj
@@ -11,10 +11,16 @@
   (let [[_ cmd n] (first (re-seq #"([NWSELRF])(\d+)" s))]
     [(keyword cmd) (Integer/parseInt n)]))
 
+(defn parse-2 [s]
+  (let [[_ cmd n] (first (re-seq #"([NWSELRF])(\d+)" s))]
+    [(keyword (str cmd "2")) (Integer/parseInt n)]))
+
 (def initial-state 
   {:facing :E
    :x 0
-   :y 0})
+   :y 0
+   :wpx 10
+   :wpy 1})
 
 (def left-turns {:N :W, :W :S, :S :E, :E :N})
 (def right-turns {:N :E, :E :S, :S :W, :W :N})
@@ -29,15 +35,43 @@
     facing
     (turn-right (right-turns facing) (- amount 90))))
 
+(defn turn-wp-left [{:keys [wpx wpy] :as state} amount]
+  (if (<= amount 0) 
+    state
+    (turn-wp-left 
+      (-> state
+        (assoc :wpx (- wpy))
+        (assoc :wpy wpx)) 
+      (- amount 90))))
+
+(defn turn-wp-right [{:keys [wpx wpy] :as state} amount]
+  (if (<= amount 0) 
+    state
+    (turn-wp-right
+      (-> state
+        (assoc :wpx wpy)
+        (assoc :wpy (- wpx)))
+      (- amount 90))))
+
 (defn advance [state [cmd n]]
   (condp = cmd
     :N (update state :y #(+ % n))
     :S (update state :y #(- % n))
-    :W (update state :x #(+ % n))
-    :E (update state :x #(- % n))
+    :W (update state :x #(- % n))
+    :E (update state :x #(+ % n))
     :F (advance state [(:facing state) n])
     :R (update state :facing #(turn-right % n))
-    :L (update state :facing #(turn-left % n))))
+    :L (update state :facing #(turn-left % n))
+
+    :N2 (update state :wpy #(+ % n))
+    :S2 (update state :wpy #(- % n))
+    :W2 (update state :wpx #(- % n))
+    :E2 (update state :wpx #(+ % n))
+    :F2 (-> state 
+          (update :x #(+ % (* n (:wpx state))))
+          (update :y #(+ % (* n (:wpy state)))))
+    :R2 (turn-wp-right state n)
+    :L2 (turn-wp-left state n)))
 
 (defn get-answer [{:keys [x y]}]
   (+ (abs x) (abs y)))
@@ -49,12 +83,18 @@
      (h/slurp-strings file)
      (map parse)
      (reduce advance initial-state)
-     get-answer
-     ; ...
-     )))
+     get-answer)))
 
-(solve-1 "resources/2020/day12.test.txt")
+(defn solve-2
+  ([] (solve-2 "resources/2020/day12.txt"))
+  ([file]
+   (->>
+     (h/slurp-strings file)
+     (map parse-2)
+     (reduce advance initial-state)
+     get-answer)))
 
 (deftest test-stuff [] 
   (test/are [x y] (= x y)
-    25 (solve-1 "resources/2020/day12.test.txt")))
+    25 (solve-1 "resources/2020/day12.test.txt")
+    286 (solve-2 "resources/2020/day12.test.txt")))
