6ec35d65daa59c1473fc0a1975c222b09ec10326
Einārs Lielmanis <einars@spicausis.lv>
2023-01-03 10:26:37 +0200

    2020/13.2

--- src/aoc_2020/day13.clj
+++ src/aoc_2020/day13.clj
@@ -9,14 +9,38 @@
 
 (defn parse-schedule [sched]
   (->> (str/split sched #",")
-    (filter (partial not= "x" ))
-    (map #(Integer/parseInt %))))
+    (map #(when (not= "x" %) (Integer/parseInt %)))))
 
 (defn parse [[n sched]]
   [(Integer/parseInt n) (parse-schedule sched)])
 
 (defn get-wait-times [[n sched]]
-  (map (fn [bus] [bus (- bus (mod n bus))]) sched))
+  (map (fn [bus] [bus (- bus (mod n bus))]) (keep identity sched)))
+
+(defn x-mod-n 
+  ; find first x so that there exists x * n = rem (mod q)
+  ([n q rem] (x-mod-n n q rem 1))
+  ([n q rem x] 
+   (cond 
+     (= (mod (* x n) q) rem) x
+     :else (recur n q rem (inc x)))))
+
+(defn mod-solve 
+  "
+  a) X = a * n + b
+  b) X = -p (mod q) 
+  "
+  [[a b] [p q]]
+  (let [np (mod (- 0 p b) q)
+        mult (x-mod-n a q np)]
+    ; a * n = - p - b (mod q)
+    [(* q a) (+ b (* mult a))]))
+
+(defn s2 [xs]
+  (let [[_ n] (first xs)
+        xs (next xs)
+        xs (filter #(some? (second %)) xs)]
+    (second (reduce mod-solve [n 0] xs))))
 
 (defn solve-1
   ([] (solve-1 "resources/2020/day13.txt"))
@@ -29,6 +53,25 @@
      first
      (apply *))))
 
+(defn solve-2
+  ([] (solve-2 "resources/2020/day13.txt"))
+  ([file]
+   (->>
+     (h/slurp-strings file)
+     parse
+     second
+     vec
+     h/indexed
+     s2)))
+
+;(solve-2 "resources/2020/day13.test.txt")
+;(x-mod-n 7 0)
+;(x-mod-n 17 13 2)
+
+
 (deftest test-stuff [] 
   (test/are [x y] (= x y)
-    295 (solve-1 "resources/2020/day13.test.txt")))
+    754018 (s2 (h/indexed [67 7 59 61]))
+    779210 (s2 (h/indexed [67 nil 7 59 61]))
+    295 (solve-1 "resources/2020/day13.test.txt")
+    1068781 (solve-2 "resources/2020/day13.test.txt")))
