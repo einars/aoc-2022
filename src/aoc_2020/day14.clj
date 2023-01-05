21d84d10a9c3d41025a537a0da1f8826c8214717
Einārs Lielmanis <einars@spicausis.lv>
2023-01-05 22:03:18 +0200

    2020/14.2

--- src/aoc_2020/day14.clj
+++ src/aoc_2020/day14.clj
@@ -7,13 +7,15 @@
     [clojure.pprint :as pp]
     [aoc.helpers :as h]))
 
+(def ^:dynamic *v2* false)
+
 (defn extract-mask [s]
   (let [[bits] (re-seq #"[01X]+$" s)
         bits (h/indexed (reverse bits))]
     (keep #(condp = (second %)
              \1 [(first %) 1]
-             \0 [(first %) 0]
-             \X nil) bits)))
+             \0 (if *v2* nil [(first %) 0])
+             \X (when *v2* [(first %) :float])) bits)))
 
 (defn parse-mem [s]
   (let [[[_ addr data]] (re-seq #"^mem\[(\d+)\] = (\d+)$" s)]
@@ -35,6 +37,24 @@
     :mask (assoc state :mask (cmd :mask))
     :mem (update state :mem #(assoc % (:addr cmd) (masked (:data cmd) (:mask state))))))
 
+(defn masked-addrs [addr mask]
+
+  (if-let [m0 (first mask)]
+    (let [[n bit] m0]
+      (condp = bit
+        1 (masked-addrs (bit-set addr n) (rest mask))
+        :float (concat 
+                 (masked-addrs (bit-clear addr n) (rest mask))
+                 (masked-addrs (bit-set addr n) (rest mask)))))
+    [addr]))
+
+(defn exec-2 [state cmd]
+  (condp = (:cmd cmd)
+    :mask (assoc state :mask (cmd :mask))
+    :mem (update state :mem #(reduce (fn [mem addr] 
+                                       (assoc mem addr (:data cmd)))
+                               %
+                               (masked-addrs (:addr cmd) (:mask state))))))
 
 (defn solve-1
   ([] (solve-1 "resources/2020/day14.txt"))
@@ -48,6 +68,20 @@
      (reduce +))))
 
 
+(defn solve-2
+  ([] (solve-2 "resources/2020/day14.txt"))
+  ([file]
+   (binding [*v2* true]
+     (->>
+       (h/slurp-strings file)
+       (map parse)
+       (reduce exec-2 {:mask [] :mem {}})
+       :mem
+       vals
+       (reduce +)))))
+
+
 (deftest test-stuff [] 
   (test/are [x y] (= x y)
-    165 (solve-1 "resources/2020/day14.test.txt")))
+    165 (solve-1 "resources/2020/day14.test.txt")
+    208 (solve-2 "resources/2020/day14.test2.txt")))
