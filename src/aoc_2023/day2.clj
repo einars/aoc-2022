1260d1f9a1cf6153950ee4650291eab2c1f92c5a
Einārs Lielmanis <einars@spicausis.lv>
2023-12-02 10:39:38 +0200

    tests

--- src/aoc_2023/day2.clj
+++ src/aoc_2023/day2.clj
@@ -9,8 +9,7 @@
                               :blue 14 })
 
 
-(def sample-games [
-                   "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+(def sample-games ["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
                    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
                    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
                    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
@@ -61,15 +60,12 @@
 (deftest tests []
   (are [x y] (= x y)
     {:green 8} (parse-count "8 green")
-    {:game 1, :reveals [
-                        {:blue 3 :red 4}
+    {:game 1, :reveals [{:blue 3 :red 4}
                         {:red 1 :green 2 :blue 6}
-                        {:green 2}
-                        ]}
+                        {:green 2}]}
     (parse-line "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
     8 (solve-1 sample-games)
-    8 (solve-2 sample-games)
-    ))
+    2286 (solve-2 sample-games)))
 
 (comment
   (solve-1)
--- test/aoc/core_test.clj
+++ test/aoc/core_test.clj
@@ -1,8 +1,8 @@
 (ns aoc.core-test
   (:require [clojure.test :refer [run-tests]]))
 
-(apply run-tests (for [y (range 2015 2024)
-                       d (range 26)
+(apply run-tests (for [y [2023] ; (range 2015 2024)
+                       d (range 1 26)
                        :let [n (symbol (format "aoc-%d.day%d" y d))]
                        :when (try (require n) (ns-publics n) (catch Exception _ nil))]
                    n))
