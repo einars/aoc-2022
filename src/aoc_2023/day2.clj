d1145c5d13db54faa8e1d98d5cf08a3a8e904cd9
Einārs Lielmanis <einars@spicausis.lv>
2023-12-03 00:27:51 +0200

    Use insta/parser

--- src/aoc_2023/day2.clj
+++ src/aoc_2023/day2.clj
@@ -1,7 +1,7 @@
 (ns aoc-2023.day2
   (:require
-    [clojure.test :as test :refer [deftest is are]]
-    [protoflex.parse :as p :refer (sep-by parse series integer word string-in)]
+    [clojure.test :as test :refer [deftest are]]
+    [instaparse.core :as insta]
     [aoc.helpers :as h]))
 
 (def ^:dynamic *initial-bag* {:red 12
@@ -15,28 +15,30 @@
                    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
                    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green" ])
 
-(defn parse-color [] (string-in ["red" "blue" "green"]))
+(def game-parser (insta/parser
+                   "
+    <game> = <'Game '> int <': '> reveals
+    <reveals> = reveal (<'; '> reveal)*
+    reveal = color (<', '> color)*
+    color = int <' '> ('red' | 'blue' | 'green')
+    <int> = #'\\d+'
+    "))
 
-(defn parse-reveal [] 
-  (sep-by #(series integer parse-color) #(p/chr \,) #(p/chr \;)))
+; (game-parser "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+;("1" 
+;  [:reveal [:color "3" "blue"] [:color "4" "red"]]
+;  [:reveal [:color "1" "red"] [:color "2" "green"] [:color "6" "blue"]]
+;  [:reveal [:color "2" "green"]])
 
-(defn process-reveals [rs]
-  (->> rs 
-    (mapv (fn [[a b]] [(keyword b) a]))
-    (into {})))
 
-(defn parse-reveals []
-  (->> (p/multi+ parse-reveal)
-    (mapv process-reveals)))
+(defn make-reveal [colors] 
+  (into {} (mapv (fn [[_ n k]] [(keyword k) (Integer/parseInt n)]) (rest colors))))
 
-(defn parse-game []
-  (let [[_ game _ reveals] (series #(word "Game") integer #(word ":") parse-reveals)]
-    {:game game
-     :reveals reveals}))
-  
-;(parse parse-game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+(defn parse-game [g]
+  (let [[game & reveals] (game-parser g)]
+    {:game (Integer/parseInt game)
+     :reveals (mapv make-reveal reveals)}))
 
-(defn string-to-game [l] (parse parse-game l))
 
 (defn reveal-possible? [reveal]
   (every? (fn [[color count]] (>= (color *initial-bag*) count)) reveal))
@@ -55,7 +57,7 @@
   ([] (solve-1 (h/slurp-strings "resources/2023/day2.txt")))
   ([lines]
    (->> lines
-     (mapv string-to-game)
+     (mapv parse-game)
      (filter game-possible?)
      (mapv :game)
      (reduce +))))
@@ -65,7 +67,7 @@
   ([] (solve-2 (h/slurp-strings "resources/2023/day2.txt")))
   ([lines]
    (->> lines
-     (mapv string-to-game)
+     (mapv parse-game)
      (mapv get-minimum-score)
      (reduce +))))
 
@@ -74,7 +76,7 @@
     {:game 1, :reveals [{:blue 3 :red 4}
                         {:red 1 :green 2 :blue 6}
                         {:green 2}]}
-    (string-to-game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+    (parse-game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
     8 (solve-1 sample-games)
     2286 (solve-2 sample-games)))
 
