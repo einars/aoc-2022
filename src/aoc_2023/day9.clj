d1b265ad0b12c7f0fd25a4aaa983d9872b5bd2b3
Einārs Lielmanis <einars@spicausis.lv>
2023-12-09 12:48:40 +0200

    pt2

--- src/aoc_2023/day9.clj
+++ src/aoc_2023/day9.clj
@@ -13,32 +13,50 @@
 (defn as-int-list [s]
   (map parse-long (str/split s #" ")))
 
-(defn count-solution [accu]
+(defn count-solution-1 [accu]
   (reduce + (map last accu)))
+
+(defn count-solution-2
+  ([accu] (count-solution-2 (reverse accu) 0))
+  ([accu sol]
+   (if (seq accu)
+     (recur (next accu) (- (first (first accu)) sol))
+     sol)))
+
+(def ^:dynamic *count-op* count-solution-1)
 
 (defn predict 
   ([xs] (predict xs []))
   ([xs accu]
    (if (apply = xs)
-     (count-solution (conj accu xs))
+     (*count-op* (conj accu xs))
      (recur (map (fn [[a b]] (- b a)) (partition 2 1 xs)) (conj accu xs)))))
 
-(predict [0 3 6 9 12 15])
+(binding [*count-op* identity]
+  (predict [0 3 6 9 12 15])
+  )
 
 (defn solve-1
   ([] (solve-1 (h/slurp-strings input-file)))
-  ([m] (->> m
-         )))
+  ([m] (->> m 
+         (map as-int-list)
+         (map predict)
+         (reduce +))))
+
 
 (defn solve-2
   ([] (solve-2 (h/slurp-strings input-file)))
-  ([m] (->> m
-         )))
+  ([m] (binding [*count-op* count-solution-2] 
+         (solve-1 m))))
 
 (deftest test-stuff [] 
   (are [x y] (= x y)
-    0 (solve-1 sample-data)
-    0 (solve-2 sample-data)))
+    12 (predict [0 3 6 9 12 15])
+    -3 (binding [*count-op* count-solution-2] (predict [0 3 6 9 12 15]))
+    5 (binding [*count-op* count-solution-2] (predict [10 13 16 21 30 45]))
+    114 (solve-1 sample-data)
+    2 (solve-2 sample-data)
+    ))
 
 (comment
   (solve-1)
