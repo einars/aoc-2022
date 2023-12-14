b699ead7d7137f4b3b16fe0d82e7eeb6f004b433
Einārs Lielmanis <einars@spicausis.lv>
2023-12-14 13:30:31 +0200

    day 13 pt 2

--- src/aoc_2023/day13.clj
+++ src/aoc_2023/day13.clj
@@ -3,10 +3,12 @@
     [clojure.test :as test :refer [deftest are]]
     [clojure.string :as str]
     [clojure.pprint :as pprint]
+    [clojure.set :as set]
     [aoc.helpers :as h]))
 
 (def sample-input "resources/2023/day13.sample.txt")
 (def input-file "resources/2023/day13.txt")
+
 
 (defn is-mirror? [elts n]
   (let [[a b] (split-at n elts)
@@ -15,10 +17,26 @@
         b (take len b)]
     (when (= a b) n)))
 
+
+(defn is-smudged-mirror? [elts n]
+  (let [[a b] (split-at n elts)
+        len (min (count a) (count b))
+        a (take len (reverse a))
+        b (take len b)
+        diffs (map (fn [a b] (+ (count (set/difference a b)) (count (set/difference b a)))) a b)
+        ;_ (prn n :a a)
+        ;_ (prn n :b b)
+        ;_ (prn n :diffs diffs)
+        ]
+    (when (= 1 (reduce + diffs)) n)))
+
+(def ^:dynamic *mirror-test* is-mirror?)
+
 (defn find-mirror [elts]
   (some
-    #(is-mirror? elts %)
+    #(*mirror-test* elts %)
     (range 1 (count elts))))
+
 
 (defn setize-map [m]
   (let [{:keys [xmin xmax ymin ymax]} (h/map-dimensions m)]
@@ -60,16 +78,29 @@
          (reduce +))))
 
 (defn solve-2
-  ([] (solve-2 (h/slurp-strings input-file)))
-  ([m] (->> m
-         )))
+  ([] (solve-2 (read-maps input-file)))
+  ([m] (binding [*mirror-test* is-smudged-mirror?] (solve-1 m))))
+
+(def xx (str/trim "
+##.###....##.#..#
+##.###....##.#..#
+#.....###..##....
+.#####.#..#.####.
+#..#.#...#....#.#
+##.#.#...#....#.#
+.#####.#..#.####.
+  "))
 
 ;(find-mirror (:cols (first (read-maps sample-input))))
+(binding [*mirror-test* is-smudged-mirror?] (find-mirror (:rows (read-map xx))))
+
+
+
 
 (deftest test-stuff [] 
   (are [x y] (= x y)
     405 (solve-1 (read-maps sample-input))
-    0 (solve-2 sample-input)))
+    0 (solve-2 (read-maps sample-input))))
 
 (comment
   (solve-1)
