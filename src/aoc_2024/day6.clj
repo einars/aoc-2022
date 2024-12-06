dba4571d9391d2d74f727be521bb3109c4d21ca3
Einārs Lielmanis <einars@spicausis.lv>
2024-12-06 10:31:14 +0200

    6.2

--- src/aoc_2024/day6.clj
+++ src/aoc_2024/day6.clj
@@ -1,6 +1,7 @@
 (ns aoc-2024.day6
   (:require
    [clojure.test :as test :refer [deftest is are]]
+   [clojure.tools.trace :refer :all]
    [clojure.string :as str]
    [aoc.helpers :as h]))
 
@@ -21,15 +22,13 @@
 
 (defn parse-input [s]
   (let [[area dimensions] (-> s (str/split #"\n") h/make-xy-map)
-        guard-pos (first (filter (comp #{\^} area) (keys area)))]
+        pos (first (filter (comp #{\^} area) (keys area)))]
     {:dimensions dimensions
-     :guard-pos guard-pos
+     :pos pos
      :direction :up
-     :visited #{guard-pos}
-     :obstcacles (set (keys (dissoc area guard-pos)))
-     }))
+     :visited #{pos}
+     :obstacles (set (keys (dissoc area pos)))}))
 
-(parse-input sample)
 
 (def turn-right 
   {:up :rt
@@ -39,30 +38,96 @@
 
 (defn went-outside?
   [{:keys [dimensions] :as task} {:keys [x y] :as pos}]
-
-
-  )
+  (or
+    (< x 0)
+    (< y 0)
+    (>= x (:x dimensions))
+    (>= y (:y dimensions)) ))
 
 (defn walk-until-outside
-  [{:keys [direction guard-pos obstacles visited] :as task}]
+  ([task] (walk-until-outside task #{}))
+  ([{:keys [direction pos obstacles visited] :as task} turns]
 
-  (let [npos (h/move guard-pos direction)]
-    (if (obstacles npos)
-      (recur (update task :direction turn-right))
+   (prn :task task :turns turns)
 
-      (if (went-outside? task npos)
-        visited
-        (recur (-> task
-                 (assoc :guard-pos npos)
-                 (update :visited #(conj % npos))))))))
+   (let [npos (h/move pos direction)]
+     (cond
+
+       (turns [pos direction]) nil
+
+       (obstacles npos) (recur (update task :direction turn-right) (conj turns [pos direction]))
+
+       (went-outside? task npos) visited
+
+       :else (recur (-> task
+                      (assoc :pos npos)
+                      (update :visited #(conj % npos)))
+               turns )))))
+
+
+(defn boxen-solve
+  ([task]
+   (let [path (walk-until-outside task)
+         ; skip initial pos
+         path (disj path (:pos task))]
+
+     (filter (fn [pos] (walk-until-outside (update task :obstacles #(conj % pos))))) path)))
+
+(boxen-solve (parse-input sample))
+(count (boxen-solve (parse-input sample)))
+
+(comment defn would-loop?
+  [{:keys [obstacles pos direction] :as task} visited]
+  (let [npos (h/move pos direction)]
+    (cond
+      (get visited [npos direction]) true
+      (get obstacles npos) (recur (update task :direction turn-right) visited)
+      (went-outside? task npos) false
+      :else (recur 
+              (assoc task :pos npos)
+              (conj visited [npos direction]) ) ) ) )
+
+(comment defn walk-gather-loops
+  ([task] (walk-gather-loops task #{[(:pos task) :up]} []))
+  ([{:keys [direction pos obstacles visited] :as task} visited-directions accum]
+
+   (let [npos (h/move pos direction)]
+     (cond
+       (went-outside? task npos) accum
+
+       (get obstacles npos) (recur 
+                              (update task :direction turn-right)
+                              (conj visited-directions [pos direction])
+                              accum)
+
+       :else (recur
+               (-> task
+                 (assoc :pos npos)
+                 (update :visited #(conj % npos)))
+               
+               (conj visited-directions [npos direction])
+
+               (if (and
+                     (not (visited npos))
+                     (would-loop? 
+                       (-> task 
+                         (update :direction turn-right)
+                         (update :obstacles (conj npos)))
+                       visited-directions))
+                 (conj accum npos)
+                 accum))))))
 
 (defn pt1
   [task]
-  0)
+  (count (walk-until-outside task)))
 
 (defn pt2
   [task]
-  0)
+  (count (walk-gather-loops task)))
+
+;(walk-gather-loops (parse-input sample))
+(walk-until-outside (parse-input sample))
+
 
 (defn solve-1
   ([] (solve-1 (slurp input)))
@@ -74,8 +139,8 @@
 
 (deftest tests []
   (are [x y] (= x y)
-    1 (pt1 (parse-input sample))
-    2 (pt2 (parse-input sample))))
+    41 (pt1 (parse-input sample))
+    6 (pt2 (parse-input sample))))
 
 (comment
   (solve-1)
