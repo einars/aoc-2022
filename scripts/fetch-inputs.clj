32a50cb20c8762a8ec0c10657b789d067e54ba69
Einārs Lielmanis <einars@spicausis.lv>
2024-12-18 18:47:28 +0200

    Do not fetch into the future

--- scripts/fetch-inputs.clj
+++ scripts/fetch-inputs.clj
@@ -3,6 +3,8 @@
 (require '[babashka.http-client :as http])
 (require '[babashka.fs :as fs])
 
+(import java.time.LocalDate)
+
 (def session-id (first *command-line-args*))
 
 (when-not session-id
@@ -13,17 +15,20 @@
 (def basedir 
   (.toString (.getParent (.getParent (.normalize (fs/path *file*))))))
 
-(doseq [year (range 2022 2025)]
-  (doseq [day (range 1 25)]
-    (let [url (format "https://adventofcode.com/%d/day/%d/input" year day)
-          dir  (format "%s/resources/%d/" basedir year)
-          file (format "%s/resources/%d/day%d.txt" basedir year day)]
-      (when-not (fs/exists? file)
-        (let [input (-> (http/get url 
-                      {:headers 
-                       {:cookie (format "session=%s" session-id)}})
-            :body)]
-          (prn file)
-          (fs/create-dirs dir)
-          (spit file input))))))))
+(let [this-year (.getYear (LocalDate/now))
+      this-date (.getDayOfMonth (LocalDate/now))]
+  (doseq [year (range 2022 2025)]
+    (doseq [day (range 1 25)]
+      (let [url (format "https://adventofcode.com/%d/day/%d/input" year day)
+            dir  (format "%s/resources/%d/" basedir year)
+            file (format "%s/resources/%d/day%d.txt" basedir year day)]
+        (when (or (< year this-year) (<= day this-date))
+          (when-not (fs/exists? file)
+            (let [input (-> (http/get url 
+                          {:headers 
+                          {:cookie (format "session=%s" session-id)}})
+                :body)]
+              (prn file)
+              (fs/create-dirs dir)
+              (spit file input))))))))
 
